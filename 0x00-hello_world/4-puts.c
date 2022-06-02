694d4ffbe942e76cebb31dd15568480deaa7328d
liliyosa <liliyosambakureya@gmail.com>
2022-06-02 12:02:43 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Prints a text
  *
- *   * main - Prints a text
+ * Return: Always 0 (Success)
  *
- *     *
- *
- *       * Return: Always 0 (Success)
- *
- *         */
-
+ */
 int main(void)
-
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
