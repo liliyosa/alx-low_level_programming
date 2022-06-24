7a98aa8c0ac5f0eb2f5d7211a9deb8deb1dab401
liliyosa <liliyosambakureya@gmail.com>
2022-06-24 03:00:04 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Prints the program name, followed by a new line.
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int __attribute__((__unused__)) argc, char *argv[])
