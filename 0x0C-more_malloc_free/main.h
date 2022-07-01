fae03d48d6cbd38908d319125037b01005c4c744
liliyosa <liliyosambakureya@gmail.com>
2022-07-01 05:16:38 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
