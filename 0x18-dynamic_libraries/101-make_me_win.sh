8ed0b7a912554b24005e88255b4df91277b10024
liliyosa <liliyosambakureya@gmail.com>
2022-09-05 15:34:55 -0700

    updated commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/liliyosa/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+wget -P /tmp/ https://raw.github.com/liliyosa/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
